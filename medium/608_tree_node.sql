4f67c5735780bf944f5a97b8b42504772b134823
Lindert Van de Poel <34005648+Lindertvdp@users.noreply.github.com>
2023-02-16 06:50:09 +0100

    Update 608_tree_node.sql

--- medium/608_tree_node.sql
+++ medium/608_tree_node.sql
@@ -2,12 +2,12 @@ select id,case
             when id = (select id 
                        from Tree 
                        where p_id is null) 
-                then Root 
+                then "Root" 
             when p_id is not null 
                       and (select count(1) 
                            from Tree t2 
                            where t2.p_id = t1.id) > 0 
-                then Inner
-            else Leaf
+                then "Inner"
+            else "Leaf"
           end as type
 from Tree t1
